3430cdc06af7c77058363bbb432b9ae1736624c5
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-10-20 21:38:55 +1100

    Commit File

---
+++ src/main/java/com/dellnaresh/entity/Exchangerate.java
@@ -0,0 +1,68 @@
+package com.dellnaresh.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Created by nareshm on 20/10/2015.
+ */
+@Entity
+public class Exchangerate {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
+    private String fromcurrency;
+
+    private String tocurrency;
+
+    private double rate;
+
+    private Date currentdate;
+
+    public Exchangerate(){
+
+    }
+
+    public Exchangerate(String fromcurrency, String tocurrency, double rate, Date currentdate) {
+        this.fromcurrency = fromcurrency;
+        this.tocurrency = tocurrency;
+        this.rate = rate;
+        this.currentdate = currentdate;
+    }
+
+    public String getFromcurrency() {
+        return fromcurrency;
+    }
+
+    public void setFromcurrency(String fromcurrency) {
+        this.fromcurrency = fromcurrency;
+    }
+
+    public String getTocurrency() {
+        return tocurrency;
+    }
+
+    public void setTocurrency(String tocurrency) {
+        this.tocurrency = tocurrency;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public Date getCurrentdate() {
+        return currentdate;
+    }
+
+    public void setCurrentdate(Date currentdate) {
+        this.currentdate = currentdate;
+    }
+}
