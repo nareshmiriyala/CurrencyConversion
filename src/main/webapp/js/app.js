6d9ec84beba238c5854a25fa944cd718eed7aac0
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-11-06 20:23:45 +1100

    commit files

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,87 +1,144 @@
-var app=angular.module("app",[]);
-//app.config(function ($routeProvider){
-//   $routeProvider.when('/',{
-//       templateUrl:'/home.html',
-//       controller:'homeController'
-//   }).when('/exchangerate',{
-//       templateUrl: 'pages/exchangerate.html',
-//       controller:'exchangeController'
-//   }) .when('/about',{
-//       templateUrl: 'pages/about.html',
-//       controller: 'aboutController'
-//   });
-//});
-//app.controller("homeController",function($scope){
-//   console.log("Naresh home") ;
-//});
-//app.controller("aboutController",function($scope){
-//   console.log("About test") ;
-//});
-//var note={  id:123,
-//            name:"note1",
-//            content:"contect"};
-// 
-//        app.filter('stripdashes', function () {
-//    // the function we are in returns
-//    // the function below
-//    return function(txt) {
-//        return txt.split('-').join(' ');
-//    };
-//
-//});
-//app.controller("myctrl",function ($scope){
-//   console.log("Test") ;
-//   $scope.note1=note;
-//   $scope.wname="Naresj-Test";
-//});
-//app.factory('addnoteserivce',function($http){
-//    var noteservices={};
-//    noteservices.addnote=function(note){
-//        var promise=$http({method: 'POST', url: '/addnotes', data: note});
-//        return promise;
-//    };
-//    return noteservices;
-//});
-//
-//app.controller("addnotesctrl",function ($scope,addnoteserivce){
-//    $scope.addnote=function(){
-//        var note={id:$scope.note.id,
-//                   name:$scope.note.name,
-//                   content:$scope.note.content};
-//      var promise=addnoteserivce.addnote(note)  ;
-//    };
-//});
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
 
-//app.factory('getrateservice',function($http){
-//   var rateservice={};
-//  rateservice.getrate=function (ratedata){
-//       console.log("Getting rate data");
-//      var promise=$http(
-//              {method:'POST',
-//                 url:'/getrate',
-//                 data:ratedata
-//             }
-//             );
-//        return promise;
-//  };
-//    return rateservice;
-//});
-//app.controller("exchangeController",function($scope){
-//    console.log("Getting rate");
-//    var ratedata={fromCurrency:$scope.fromCurrency,
-//                  toCurrency:$scope.toCurrency};
-//   var promise=getrateservice.getrate(ratedata);
-//});
-app.controller('exchangeController', ['$http', function($http) {
-var self = this;
-self.items = [];
-self.ratedata = {};
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('conversionApp', [
+    'ngRoute', 'angular-loading-bar'
+]);
 
-self.getrate = function() {
-$http.post('/getrate', self.ratedata)
-.then(function(response) {
-self.rate=response.data;
-console.log(self.rate);
-});
-};
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        // Home
+        .when("/", {templateUrl: "pages/home.html", controller: "PageCtrl"})
+        // Pages
+        .when("/about", {templateUrl: "pages/about.html", controller: "PageCtrl"})
+        .when("/faq", {templateUrl: "pages/faq.html", controller: "PageCtrl"})
+        .when("/pricing", {templateUrl: "pages/pricing.html", controller: "PageCtrl"})
+        .when("/videodownload", {templateUrl: "pages/videodownload.html", controller: "downloadCtrl"})
+        .when("/exchangerate", {templateUrl: "pages/exchangerate.html", controller: "exchangeController"})
+        .when("/ratechart", {templateUrl: "pages/ratechart.html", controller: "rateController"})
+        .when("/services", {templateUrl: "pages/services.html", controller: "PageCtrl"})
+        .when("/contact", {templateUrl: "pages/contact.html", controller: "PageCtrl"})
+        // Blog
+        .when("/blog", {templateUrl: "pages/blog.html", controller: "BlogCtrl"})
+        .when("/blog/post", {templateUrl: "pages/blog_item.html", controller: "BlogCtrl"})
+        // else 404
+        .otherwise("/404", {templateUrl: "pages/404.html", controller: "PageCtrl"});
 }]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', blogController);
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', pageController);
+
+app.controller('exchangeController', ['$http', exchangeController]);
+
+app.controller('downloadCtrl', ['$http', downloadController]);
+app.controller('rateController', ['$http', function ($http) {
+    var self = this;
+    self.items = [];
+    self.rateResp = {};
+
+    self.getrate = function () {
+        console.log("Rate History input:" + self.rate.startdate)
+        $http.post('/gethistory', self.rate)
+            .then(function (response) {
+                self.rateResp = response.data;
+                console.log(self.rateResp);
+                console.log(self.rateResp[0].to)
+                createChart(response.data);
+            });
+    };
+    self.addrate = function () {
+        console.log("addrate called");
+        $http.post('/addrate', self.addedrate).then(
+            function (response) {
+                self.message = "Success";
+            }, function (response) {
+                self.message = "Failure to add";
+            }
+        );
+    };
+
+}]);
+var randomScalingFactor = function () {
+    return Math.round(Math.random() * 100 * (Math.random() > 0.5 ? -1 : 1));
+};
+var randomColorFactor = function () {
+    return Math.round(Math.random() * 255);
+};
+var randomColor = function (opacity) {
+    return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',' + (opacity || '.3') + ')';
+};
+function createChart(items) {
+    console.log("Item data:" + items);
+    var dateList = [];
+    var rateList = [];
+    for (var par in items) {
+        var dateValue = moment(items[par].currentdate).format('MM/DD/YYYY');
+        console.log("Date Value:" + dateValue);
+        dateList.push(dateValue);
+        rateList.push(items[par].rate);
+    }
+    var config = {
+        type: 'line',
+        data: {
+            labels: dateList,
+            datasets: [
+                {
+                    label: "My First dataset",
+                    data: rateList,
+                    fill: false,
+                    borderDash: [5, 5]
+                }
+            ]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                xAxes: [
+                    {
+                        display: true,
+                        scaleLabel: {
+                            show: true,
+                            labelString: 'Date'
+                        }
+                    }
+                ],
+                yAxes: [
+                    {
+                        display: true,
+                        scaleLabel: {
+                            show: true,
+                            labelString: 'Rate'
+                        }
+                    }
+                ]
+            }
+        }
+    };
+
+    $.each(config.data.datasets, function (i, dataset) {
+        dataset.borderColor = randomColor(0.4);
+        dataset.backgroundColor = randomColor(0.5);
+        dataset.pointBorderColor = randomColor(0.7);
+        dataset.pointBackgroundColor = randomColor(0.5);
+        dataset.pointBorderWidth = 1;
+    });
+
+    console.log(config.data);
+    var ctx = document.getElementById("canvas").getContext("2d");
+    window.myLine = new Chart(ctx, config);
+}
